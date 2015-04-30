d3fed9a06cf6f1951d59019a90a06f39c40b3865
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2015-04-30 21:09:41 +0000

    Adding NA2 OSCARS plugin

---
+++ orca-external/na2-plugins/na2-oscars-lib/src/main/java/orca/nodeagent2/oscarslib/driver/ClientConfig.java
@@ -0,0 +1,20 @@
+package orca.nodeagent2.oscarslib.driver;
+
+
+
+public class ClientConfig {
+
+
+    protected String keyPassword = null;
+
+
+
+    public String getKeyPassword() {
+        return keyPassword;
+    }
+
+    public void setKeyPassword(String keyPassword) {
+        this.keyPassword = keyPassword;
+    }
+}
+
---
+++ orca-external/na2-plugins/na2-oscars-lib/src/main/java/orca/nodeagent2/oscarslib/driver/ClientConfigHolder.java
@@ -0,0 +1,33 @@
+package orca.nodeagent2.oscarslib.driver;
+
+import java.util.HashMap;
+
+public class ClientConfigHolder {
+    private ClientConfigHolder() {
+
+    }
+    private static ClientConfigHolder instance = null;
+    public static ClientConfigHolder getInstance() {
+        if (instance == null) {
+            instance = new ClientConfigHolder();
+        }
+        return instance;
+    }
+
+    private HashMap<String, ClientConfig> clientConfigs = new HashMap<String, ClientConfig>();
+    private ClientConfig activeClientConfig;
+
+    public ClientConfig getActiveClientConfig() {
+        return activeClientConfig;
+    }
+
+    public void addClientConfig(String url, ClientConfig cc) {
+        clientConfigs.put(url, cc);
+    }
+
+    public void setActiveClientConfig(String url) {
+        activeClientConfig = clientConfigs.get(url);
+    }
+
+
+}
